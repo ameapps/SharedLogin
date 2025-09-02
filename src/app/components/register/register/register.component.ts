5b1cb79042c9c83bcedf44d3b9d09be8258b4213
Stefano Amenta <git.stefano@outlook.it>
2025-09-02 20:13:10 +0200

    added router

--- src/app/components/register/register/register.component.ts
+++ src/app/components/register/register/register.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { User } from '../../../shared/models/user.model';
 import { CommonService } from '../../../shared/services/common/common.service';
 import { FirebaseService } from '../../../shared/services/firebase/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,7 +17,11 @@ export class RegisterComponent {
   showPassword = false;
   user: User = new User();
 
-  constructor(public common: CommonService, private fb_service: FirebaseService) {}
+  constructor(
+    public common: CommonService,
+    private fb_service: FirebaseService,
+    private router: Router
+  ) {}
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
@@ -26,14 +31,13 @@ export class RegisterComponent {
   async register() {
     try {
       this.common.lastRegisteredUser = this.user;
-      if (!this.common.appConfig) this.common.appConfig = await this.common.loadAppConfig();
+      if (!this.common.appConfig)
+        this.common.appConfig = await this.common.loadAppConfig();
       await this.fb_service.addUser(this.user);
       const hasRegistred = await this.fb_service.tryLogin(this.user);
-      if (hasRegistred) {
-        alert('Registrazione completata con successo!');
-      } else {
-        alert('Registrazione fallita. Riprova più tardi.');
-      }
+      if (hasRegistred) alert('Registrazione completata con successo!');
+      else alert('Registrazione fallita. Riprova più tardi.');
+      this.router.navigate(['/']);
     } catch (error) {
       console.error('Errore durante la registrazione:', error);
       // Gestisci l'errore di registrazione
